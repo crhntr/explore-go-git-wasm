6ae00aaa015e3980b45fc6224d858b091c76b36a
Christopher Hunter <chunter@pivotal.io>
2019-11-25 10:43:27 -0800

    start now opens browser and cleans up after itself
    Authored-by: Christopher Hunter <chunter@pivotal.io>

--- start.sh
+++ start.sh
@@ -1,6 +1,16 @@
-if [ ! -f 'wasm_exec.js' ]; then
-    cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" .
-fi
+main() {
+  trap "rm -f main.wasm wasm_exec.js" EXIT
 
-GOOS=js GOARCH=wasm go build -o main.wasm
-goexec 'http.ListenAndServe(`:8888`, http.FileServer(http.Dir(`.`)))'
+  if [ ! -f 'wasm_exec.js' ]; then
+      cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" .
+  fi
+
+  if [ ! -f 'main.wasm' ]; then
+      GOOS=js GOARCH=wasm go build -o main.wasm
+  fi
+
+  sleep 2 && open 'http://localhost:8888/' &
+  goexec 'http.ListenAndServe(`:8888`, http.FileServer(http.Dir(`.`)))'
+}
+
+main
